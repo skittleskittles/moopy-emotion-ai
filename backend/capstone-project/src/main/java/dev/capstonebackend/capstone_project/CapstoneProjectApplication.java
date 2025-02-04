00069d66eb55586f7a6ee2d174df76b9f9bea0b0
jasonliu62 <jasondev310@gmail.com>
2025-02-04 00:21:38 -0800

    [backend] springboot configuration done

--- backend/capstone-project/src/main/java/dev/capstonebackend/capstone_project/CapstoneProjectApplication.java
+++ backend/capstone-project/src/main/java/dev/capstonebackend/capstone_project/CapstoneProjectApplication.java
@@ -1,9 +1,10 @@
 package dev.capstonebackend.capstone_project;
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("dev.capstonebackend.capstone_project.dao")
 public class CapstoneProjectApplication {
 
 	public static void main(String[] args) {

b55ee62686f1dbd8dee88d9c629399e7d8f02fe1
K11yann <yiyan.kong1021@gmail.com>
2025-02-17 21:29:26 -0800

    Add Cors Config

---
+++ backend/capstone-project/src/main/java/dev/capstonebackend/capstone_project/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package dev.capstonebackend.capstone_project.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: Capstone_Project
+ * @author: Yiyan Kong
+ * @create: 2025-02-17 21:20
+ **/
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")  // 允许前端访问
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
